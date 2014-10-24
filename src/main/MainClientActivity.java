2d71eb7564d6d231467f7d679f601c2ea89d6fed
Ilter Canberk <iltercanberk@gmail.com>
2014-10-24 15:09:25 -0400

    Starter Code

---
+++ src/main/MainClientActivity.java
@@ -0,0 +1,16 @@
+package main;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainClientActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+
+	}
+
+}
