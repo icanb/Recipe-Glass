1c46b3f1c11075f63bfe661170ab3d196ac4c505
Ilter Canberk <iltercanberk@gmail.com>
2014-10-24 15:54:37 -0400

    step 2: implemented the menu

--- gen/com/example/recipe/R.java
+++ gen/com/example/recipe/R.java
@@ -22,10 +22,15 @@ public final class R {
         public static final int tuna4=0x7f020008;
     }
     public static final class id {
-        public static final int MainLabel=0x7f070000;
+        public static final int MainLabel=0x7f080000;
+        public static final int dish_1=0x7f080001;
+        public static final int dish_2=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+    }
+    public static final class menu {
+        public static final int main=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
--- src/main/MainClientActivity.java
+++ src/main/MainClientActivity.java
@@ -1,6 +1,5 @@
 package main;
 
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -8,11 +7,16 @@ import android.app.Activity;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 
 import com.example.recipe.R;
 import com.google.android.glass.app.Card;
+import com.google.android.glass.view.WindowUtils;
 import com.google.android.glass.widget.CardScrollAdapter;
 import com.google.android.glass.widget.CardScrollView;
 
@@ -41,21 +45,21 @@ public class MainClientActivity extends Activity {
 		csvCardsView.activate();
 		setContentView(csvCardsView);
 	}
-	
+
 	private class csaAdapter extends CardScrollAdapter {
 		@Override
 		public int getCount() {
-				return gCheeseCards.size();
+			return gCheeseCards.size();
 		}
 
 		@Override
 		public Object getItem(int position) {
-				return gCheeseCards.get(position);
+			return gCheeseCards.get(position);
 		}
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-				return gCheeseCards.get(position).getView();
+			return gCheeseCards.get(position).getView();
 		}
 
 		@Override
@@ -63,7 +67,7 @@ public class MainClientActivity extends Activity {
 			return 0;
 		}
 	}
-	
+
 	private void buildGrilledCheeseCards() {
 
 		for (int i = 0; i < gCheeseDirections.size(); i++) {
@@ -80,6 +84,46 @@ public class MainClientActivity extends Activity {
 			newCard.addImage(drawable);
 			gCheeseCards.add(newCard);
 		}
+	}
+
+	// Enable "ok glass" to inflate the menu
+	@Override
+	public boolean onCreatePanelMenu(int featureId, Menu menu) {
+		if (featureId == WindowUtils.FEATURE_VOICE_COMMANDS
+				|| featureId == Window.FEATURE_OPTIONS_PANEL) {
+
+			getMenuInflater().inflate(R.menu.main, menu);
+
+			return true;
+		}
+		return super.onCreatePanelMenu(featureId, menu);
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		if (featureId == WindowUtils.FEATURE_VOICE_COMMANDS
+				|| featureId == Window.FEATURE_OPTIONS_PANEL) {
+			switch (item.getItemId()) {
+			case R.id.dish_1:
+				break;
+			case R.id.dish_2:
+				break;
+			}
+			return true;
+		}
+		return super.onMenuItemSelected(featureId, item);
+	}
+
+	// Enable Tap for inflating the menu
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
+			System.out.println("Tap");
+			openOptionsMenu();
+			return true;
+		}
+
+		return super.onKeyDown(keyCode, event);
 	}
 
 }
